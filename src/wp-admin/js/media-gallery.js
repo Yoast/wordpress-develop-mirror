7ead9f4e5cde6f59fc003c938610a2d13eb4bfb1
Jip Moors <jhp.moors@gmail.com>
2016-07-14 16:47:09 +0200

    Added documentation

--- src/wp-admin/js/media-gallery.js
+++ src/wp-admin/js/media-gallery.js
@@ -1,15 +1,28 @@
 /* global ajaxurl */
+
+/**
+ * This file is used on media-upload.php which has been replaced by media-new.php and upload.php
+ * Deprecated since 3.5.0
+ */
 jQuery(function($){
+	/**
+	 * Adds an click event handler to the element with a 'wp-gallery' class.
+	 */
 	$( 'body' ).bind( 'click.wp-gallery', function(e){
 		var target = $( e.target ), id, img_size;
 
 		if ( target.hasClass( 'wp-set-header' ) ) {
+			// Opens the image to preview it full size.
 			( window.dialogArguments || opener || parent || top ).location.href = target.data( 'location' );
 			e.preventDefault();
 		} else if ( target.hasClass( 'wp-set-background' ) ) {
+			// Set the image as background of the theme.
 			id = target.data( 'attachment-id' );
 			img_size = $( 'input[name="attachments[' + id + '][image-size]"]:checked').val();
 
+			/**
+			 * This AJAX action has been deprecated since 3.5.0, see custom-background.php
+			 */
 			jQuery.post(ajaxurl, {
 				action: 'set-background-image',
 				attachment_id: id,
