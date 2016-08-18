8120abdf56b81f1a18bc4ac84f8aa941bb0055db
Jip Moors <jhp.moors@gmail.com>
2016-08-18 15:18:23 +0200

    Applied feedback

--- src/wp-admin/js/media-gallery.js
+++ src/wp-admin/js/media-gallery.js
@@ -4,11 +4,11 @@
  * This file is used on media-upload.php which has been replaced by media-new.php and upload.php
  * Deprecated since 3.5.0
  */
-jQuery(function($){
+jQuery(function($) {
 	/**
-	 * Adds an click event handler to the element with a 'wp-gallery' class.
+	 * Adds a click event handler to the element with a 'wp-gallery' class.
 	 */
-	$( 'body' ).bind( 'click.wp-gallery', function(e){
+	$( 'body' ).bind( 'click.wp-gallery', function(e) {
 		var target = $( e.target ), id, img_size;
 
 		if ( target.hasClass( 'wp-set-header' ) ) {
@@ -16,7 +16,7 @@ jQuery(function($){
 			( window.dialogArguments || opener || parent || top ).location.href = target.data( 'location' );
 			e.preventDefault();
 		} else if ( target.hasClass( 'wp-set-background' ) ) {
-			// Set the image as background of the theme.
+			// Sets the image as background of the theme.
 			id = target.data( 'attachment-id' );
 			img_size = $( 'input[name="attachments[' + id + '][image-size]"]:checked').val();
 
@@ -27,7 +27,7 @@ jQuery(function($){
 				action: 'set-background-image',
 				attachment_id: id,
 				size: img_size
-			}, function(){
+			}, function() {
 				var win = window.dialogArguments || opener || parent || top;
 				win.tb_remove();
 				win.location.reload();
