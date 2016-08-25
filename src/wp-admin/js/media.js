d5064d30806eceec5177958923ad51db7351f934
Diede Exterkate <diede@yoast.com>
2016-08-25 17:04:53 +0200

    began adding documantation to media.js

--- src/wp-admin/js/media.js
+++ src/wp-admin/js/media.js
@@ -3,6 +3,13 @@
 var findPosts;
 ( function( $ ){
 	findPosts = {
+		/**
+		 * @summary Opens a dialog to attach media to a post.
+		 *
+		 * @param {string} af_name The name of the affected element. //todo
+		 * @param {string} af_val The value of the affected post element. //todo
+		 * @returns {boolean} Always returns false.
+		 */
 		open: function( af_name, af_val ) {
 			var overlay = $( '.ui-find-overlay' );
 
@@ -14,35 +21,57 @@ var findPosts;
 			overlay.show();
 
 			if ( af_name && af_val ) {
+				// #affected is a hidden input field in the dialog that keeps track of which media should be attached.
 				$( '#affected' ).attr( 'name', af_name ).val( af_val );
 			}
 
 			$( '#find-posts' ).show();
 
+			/**
+			 * Close the dialog when the escape key is pressed.
+			 */
 			$('#find-posts-input').focus().keyup( function( event ){
 				if ( event.which == 27 ) {
 					findPosts.close();
-				} // close on Escape
+				}
 			});
 
-			// Pull some results up by default
+			// Show list of applicable posts for media attachment.
 			findPosts.send();
 
 			return false;
 		},
 
+		/**
+		 * @summary Clears the found posts lists before hiding the attach media dialog.
+		 *
+		 * @returns {void}
+		 */
 		close: function() {
 			$('#find-posts-response').empty();
 			$('#find-posts').hide();
 			$( '.ui-find-overlay' ).hide();
 		},
 
+		/**
+		 * @summary Binds a click event listener to the overlay which closes the attach media dialog.
+		 *
+		 * @returns {void}
+		 */
 		overlay: function() {
 			$( '.ui-find-overlay' ).on( 'click', function () {
 				findPosts.close();
 			});
 		},
 
+		/**
+		 * @summary Retrieves and displays posts based on the search term.
+		 *
+		 * Sends a post request to the admin_ajax.php requesting posts based on the search term provided by the user.
+		 * Defaults to all posts if no search term is provided.
+		 * 
+		 * @returns {void}
+		 */
 		send: function() {
 			var post = {
 					ps: $( '#find-posts-input' ).val(),
@@ -53,6 +82,10 @@ var findPosts;
 
 			spinner.addClass( 'is-active' );
 
+			/**
+			 * Send a POST request to admin_ajax.php, hide the spinner and replace the list of posts with the response data.
+			 * If an error occurs, display it.
+			 */
 			$.ajax( ajaxurl, {
 				type: 'POST',
 				data: post,
@@ -71,6 +104,10 @@ var findPosts;
 		}
 	};
 
+	/**
+	 * Initializes the file once the DOM is fully loaded and attaches events to the various form elements.
+	 * 
+	 */
 	$( document ).ready( function() {
 		var settings, $mediaGridWrap = $( '#wp-media-grid' );
 
@@ -85,19 +122,30 @@ var findPosts;
 			}).open();
 		}
 
+		// Prevent form submission if no post has been selected.
 		$( '#find-posts-submit' ).click( function( event ) {
 			if ( ! $( '#find-posts-response input[type="radio"]:checked' ).length )
 				event.preventDefault();
 		});
+
+		// When hitting the enter key in the search input, submit the search query.
 		$( '#find-posts .find-box-search :input' ).keypress( function( event ) {
 			if ( 13 == event.which ) {
 				findPosts.send();
 				return false;
 			}
 		});
+
+		// Bind the search button on click event.
 		$( '#find-posts-search' ).click( findPosts.send );
+
+		// Bind the close dialog click event.
 		$( '#find-posts-close' ).click( findPosts.close );
+
+		// Bind the bulk action events to the submit buttons.
 		$( '#doaction, #doaction2' ).click( function( event ) {
+
+			// Get all select elements for bulk actions that have a name starting with `action` and handle its action based on its value.
 			$( 'select[name^="action"]' ).each( function() {
 				var optionValue = $( this ).val();
 
@@ -112,7 +160,11 @@ var findPosts;
 			});
 		});
 
-		// Enable whole row to be clicked
+		/**
+		 * Enable whole row to be clickable inside the post finder.
+		 *
+		 * @returns {void}
+		 */
 		$( '.find-box-inside' ).on( 'click', 'tr', function() {
 			$( this ).find( '.found-radio input' ).prop( 'checked', true );
 		});
