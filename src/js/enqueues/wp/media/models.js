ef1be2c5bb605c49e1aa2a29719132c70b9dc067
omarreiss <omar.reiss@gmail.com>
2017-12-03 12:10:22 -0600

    Fix paths

--- src/js/enqueues/wp/media/models.js
+++ src/js/enqueues/wp/media/models.js
@@ -1,7 +1,7 @@
 window.wp = window.wp || {};
 window.wp.media = window.wp.media || {};
 
-Object.assign( window.wp.media, require( "../../../src/media/models.js" ) );
+Object.assign( window.wp.media, require( "../../../media/models.js" ) );
 
 // Clean up. Prevents mobile browsers caching
 $(window).on('unload', function(){
