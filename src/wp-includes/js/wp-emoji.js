08baf7af6a3c289f194a3d06cd9e51631c301839
NicolleY <nicolle@yoast.com>
2018-04-05 16:10:36 +0200

    First session

--- src/wp-includes/js/wp-emoji.js
+++ src/wp-includes/js/wp-emoji.js
@@ -1,5 +1,38 @@
 
 ( function( window, settings ) {
+	/**
+	 * Replaces text with Twitter emojis.
+	 *
+	 * Description. (use period)
+	 *
+	 * @since      x.x.x
+	 * @deprecated x.x.x Use new_function_name() instead.
+	 * @access     private
+	 *
+	 * @class
+	 * @augments parent
+	 * @mixes    mixin
+	 *
+	 * @alias    realName
+	 * @memberof namespace
+	 *
+	 * @see  Twitter Emoji library
+	 * @link https://github.com/twitter/twemoji
+	 * @global
+	 *
+	 * @fires   eventName
+	 * @fires   className#eventName
+	 * @listens event:eventName
+	 * @listens className~event:eventName
+	 *
+	 * @param {type}   var           Description.
+	 * @param {type}   [var]         Description of optional variable.
+	 * @param {type}   [var=default] Description of optional variable with default variable.
+	 * @param {Object} objectVar     Description.
+	 * @param {type}   objectVar.key Description of a key in the objectVar parameter.
+	 *
+	 * @return {type} Description.
+	 */
 	function wpEmoji() {
 		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver,
 
