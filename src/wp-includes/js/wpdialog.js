2e8eded2db91f7b2e55b955684fdd363848cc74d
Lisanne Kluitmans <lisanne@yoast.com>
2018-04-19 15:23:53 +0200

    Add comments to function

--- src/wp-includes/js/wpdialog.js
+++ src/wp-includes/js/wpdialog.js
@@ -1,3 +1,6 @@
+/**
+ * Wrap the jQuery UI Dialog open function to fix the TinyMCE focus.
+ */
 ( function($) {
 	$.widget('wp.wpdialog', $.ui.dialog, {
 		open: function() {
@@ -10,6 +13,7 @@
 			this._super();
 			// WebKit leaves focus in the TinyMCE editor unless we shift focus.
 			this.element.focus();
+			// Add refresh event.
 			this._trigger('refresh');
 		}
 	});
