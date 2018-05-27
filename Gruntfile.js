7b86f3d9cf6ce26b3b2bbbada9c6c72ec60127d8
Jeremy Felt <jeremyfelt@git.wordpress.org>
2018-05-27 18:46:15 +0000

    Built/Test Tools: Increase grunt watch interval on all files.
    Significantly reduces Grunt's CPU usage when `grunt watch` is in an idle/watching state.
    
    Props netweb.
    Fixes #44241.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@43324 602fd350-edb4-49c9-b593-d223f7449a82

--- Gruntfile.js
+++ Gruntfile.js
@@ -1019,6 +1019,9 @@ module.exports = function(grunt) {
 			}
 		},
 		_watch: {
+			options: {
+				interval: 2000
+			},
 			all: {
 				files: [
 					SOURCE_DIR + '**',
@@ -1029,8 +1032,7 @@ module.exports = function(grunt) {
 				tasks: ['clean:dynamic', 'copy:dynamic'],
 				options: {
 					dot: true,
-					spawn: false,
-					interval: 2000
+					spawn: false
 				}
 			},
 			'js-enqueues': {
@@ -1038,8 +1040,7 @@ module.exports = function(grunt) {
 				tasks: ['clean:dynamic', 'copy:dynamic-js', 'uglify:dynamic', 'jsvalidate:dynamic'],
 				options: {
 					dot: true,
-					spawn: false,
-					interval: 2000
+					spawn: false
 				}
 			},
 			'js-webpack': {
@@ -1051,8 +1052,7 @@ module.exports = function(grunt) {
 				tasks: ['clean:dynamic', 'webpack:dev', 'uglify:dynamic', 'jsvalidate:dynamic'],
 				options: {
 					dot: true,
-					spawn: false,
-					interval: 2000
+					spawn: false
 				}
 			},
 			config: {
@@ -1073,8 +1073,7 @@ module.exports = function(grunt) {
 				],
 				tasks: ['rtlcss:dynamic'],
 				options: {
-					spawn: false,
-					interval: 2000
+					spawn: false
 				}
 			},
 			test: {
