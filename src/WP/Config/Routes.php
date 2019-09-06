d2ede2dc2865f851a8d1f1e4e63914ca37c4b924
Joost de Valk <joost@yoast.com>
2019-09-06 14:37:49 +0200

    add dashboard action

--- src/WP/Config/Routes.php
+++ src/WP/Config/Routes.php
@@ -3,6 +3,7 @@
 namespace WP\Config;
 
 use WP\Legacy\Action\Admin\About;
+use WP\Legacy\Action\Admin\Dashboard;
 use WP\Legacy\Action\Admin\PluginInstall;
 use WP\Legacy\Action\Admin\Widgets;
 
@@ -12,6 +13,7 @@ use WP\Legacy\Action\Admin\Widgets;
  */
 class Routes {
 	const ROUTES = [
+		'wp-admin' => Dashboard::class,
 		'wp-admin/about' => About::class,
 		'wp-admin/widgets' => Widgets::class,
 		'wp-admin/plugin-install' => PluginInstall::class,
